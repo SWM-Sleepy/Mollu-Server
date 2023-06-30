8538d035e7dba9e2e66fa7c1a742552511b3d899
JunHyeongKim73 <rla5764v@gmail.com>
2023-06-30 17:23:32 +0900

    feat: initialization

---
+++ src/main/java/sleepy/mollu/server/ServerApplication.java
@@ -0,0 +1,13 @@
+package sleepy.mollu.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServerApplication.class, args);
+    }
+
+}
