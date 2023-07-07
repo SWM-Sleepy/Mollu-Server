21ac561d851f9397fad1c3b96a07c5a159e72ede
JunHyeongKim73 <rla5764v@gmail.com>
2023-07-07 12:48:20 +0900

    feat: 커스템 예외 추가

---
+++ src/main/java/sleepy/mollu/server/common/exception/AuthorizationException.java
@@ -0,0 +1,8 @@
+package sleepy.mollu.server.common.exception;
+
+public class AuthorizationException extends RuntimeException {
+
+    public AuthorizationException(String message) {
+        super(message);
+    }
+}
