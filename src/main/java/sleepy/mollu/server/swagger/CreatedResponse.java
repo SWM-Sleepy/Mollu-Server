bd60b5c68de9d79f199e38a0667a29af9bd02758
Kim Jun Hyeong <56298375+JunHyeongKim73@users.noreply.github.com>
2023-07-08 20:50:33 +0900

    feat: Swagger UI를 위한 추가 설정을 한다. (#8)
    * feat: Swagger UI를 위한 response 애노테이션 구현
    
    * fix: `OkResponse`와 `CreatedResponse` schema 제거

---
+++ src/main/java/sleepy/mollu/server/swagger/CreatedResponse.java
@@ -0,0 +1,14 @@
+package sleepy.mollu.server.swagger;
+
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target({ElementType.METHOD})
+@Retention(RetentionPolicy.RUNTIME)
+@ApiResponse(responseCode = "201", description = "Created")
+public @interface CreatedResponse {
+}
---
+++ src/main/java/sleepy/mollu/server/swagger/OkResponse.java
@@ -0,0 +1,15 @@
+package sleepy.mollu.server.swagger;
+
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target({ElementType.METHOD})
+@Retention(RetentionPolicy.RUNTIME)
+@ApiResponse(responseCode = "200", description = "OK")
+public @interface OkResponse {
+
+}
