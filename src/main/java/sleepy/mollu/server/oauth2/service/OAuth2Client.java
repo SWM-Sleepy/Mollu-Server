17d6702e9ddf9b46c0af7b1942a937b24484d9da
JunHyeongKim73 <rla5764v@gmail.com>
2023-07-12 10:38:00 +0900

    feat: 구글 ID 토큰을 처리하는 `OAuth2Client` 구현

--- src/main/java/sleepy/mollu/server/oauth2/service/OAuth2Client.java
+++ src/main/java/sleepy/mollu/server/oauth2/service/OAuth2Client.java
@@ -1,6 +1,9 @@
 package sleepy.mollu.server.oauth2.service;
 
+import java.io.IOException;
+import java.security.GeneralSecurityException;
+
 public interface OAuth2Client {
 
-    String getMemberId(String socialToken);
+    String getMemberId(String socialToken) throws GeneralSecurityException, IOException;
 }
