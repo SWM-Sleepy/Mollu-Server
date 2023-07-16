a465299dd2f7f7284c1e926a2685faf59210f8c1
Kim Jun Hyeong <56298375+JunHyeongKim73@users.noreply.github.com>
2023-07-16 19:31:10 +0900

    fix: deploy.sh 수정 (#24)

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@
 REPOSITORY=/home/ubuntu/mollu/zip
 
 echo "> 현재 구동 중인 애플리케이션 pid 확인"
-CURRENT_PID=$(pgrep -fl back-0.0.1-SNAPSHOT.jar | awk '{print $1}')
+CURRENT_PID=$(pgrep -fl server-0.0.1-SNAPSHOT.jar | awk '{print $1}')
 
 if [ -z "$CURRENT_PID" ]; then
   echo "> 현재 구동 중인 애플리케이션이 없으므로 종료하지 않습니다."
