1dffca815bffcc36fde0134df1b32cfba6140e24
JunHyeongKim73 <rla5764v@gmail.com>
2023-07-18 22:43:45 +0900

    fix: Member와 Content 관계 정리

--- src/main/java/sleepy/mollu/server/content/domain/content/Content.java
+++ src/main/java/sleepy/mollu/server/content/domain/content/Content.java
@@ -5,6 +5,7 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import sleepy.mollu.server.common.domain.BaseEntity;
+import sleepy.mollu.server.member.domain.Member;
 
 @Entity
 @Getter
@@ -28,13 +29,18 @@ public class Content extends BaseEntity {
     @Embedded
     private Location location;
 
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
     @Builder
-    public Content(String id, String contentTag, String frontContentSource, String backContentSource, String location) {
+    public Content(String id, String contentTag, String frontContentSource, String backContentSource, String location, Member member) {
         this.id = id;
         this.contentTag = new ContentTag(contentTag);
         this.frontContentSource = new ContentSource(frontContentSource);
         this.backContentSource = new ContentSource(backContentSource);
         this.location = new Location(location);
+        this.member = member;
     }
 
     public String getContentTag() {
@@ -56,5 +62,9 @@ public class Content extends BaseEntity {
     public void updateUrl(String frontContentSource, String backContentSource) {
         this.frontContentSource = new ContentSource(frontContentSource);
         this.backContentSource = new ContentSource(backContentSource);
+    }
+
+    public boolean isOwner(String memberId) {
+        return this.member.isSameId(memberId);
     }
 }
