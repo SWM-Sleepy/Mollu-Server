8aba82dd9ef6cace6fa5e1cade15cf3ba5cb0079
Kim Jun Hyeong <56298375+JunHyeongKim73@users.noreply.github.com>
2023-07-20 12:22:30 +0900

    feat: 프로필 조회 API를 구현한다. (#41)
    * feat: 멤버 getter 수정
    
    * feat: 프로필 조회 컨트롤러 구현
    
    * feat: 프로필 조회 서비스 구현
    
    * fix: 프로필 조회 컨트롤러 변경

---
+++ src/main/java/sleepy/mollu/server/member/profile/controller/ProfileController.java
@@ -0,0 +1,33 @@
+package sleepy.mollu.server.member.profile.controller;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import sleepy.mollu.server.member.profile.dto.ProfileResponse;
+import sleepy.mollu.server.member.profile.service.ProfileService;
+import sleepy.mollu.server.oauth2.controller.annotation.Login;
+import sleepy.mollu.server.swagger.*;
+
+@RestController
+@Tag(name = "알림 설정 관련 API")
+@RequestMapping("/members/profile")
+@RequiredArgsConstructor
+public class ProfileController {
+
+    private final ProfileService profileService;
+
+    @Operation(summary = "프로필 조회")
+    @OkResponse
+    @UnAuthorizedResponse
+    @NotFoundResponse
+    @InternalServerErrorResponse
+    @GetMapping
+    public ResponseEntity<ProfileResponse> searchProfile(@Login String memberId) {
+
+        return ResponseEntity.ok().body(profileService.searchProfile(memberId));
+    }
+}
