0706aa8a575d2135d0fbd046b58f1fd65dafc99a
JunHyeongKim73 <rla5764v@gmail.com>
2023-07-21 11:09:57 +0900

    feat: Entity는 BaseEntity를 상속받는다.

--- src/main/java/sleepy/mollu/server/member/domain/Preference.java
+++ src/main/java/sleepy/mollu/server/member/domain/Preference.java
@@ -4,11 +4,12 @@ import jakarta.persistence.*;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import sleepy.mollu.server.common.domain.BaseEntity;
 
 @Entity
 @Getter
 @NoArgsConstructor
-public class Preference {
+public class Preference extends BaseEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
