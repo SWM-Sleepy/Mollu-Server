7a44d98e6e7983bcf4965317251bb5a437a37511
JunHyeongKim73 <rla5764v@gmail.com>
2023-07-24 18:33:37 +0900

    feat: 그룹원 조회 컨트롤러 구현

---
+++ src/main/java/sleepy/mollu/server/group/controller/GroupController.java
@@ -0,0 +1,36 @@
+package sleepy.mollu.server.group.controller;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import sleepy.mollu.server.group.dto.GroupMemberSearchResponse;
+import sleepy.mollu.server.group.service.GroupService;
+import sleepy.mollu.server.oauth2.controller.annotation.Login;
+import sleepy.mollu.server.swagger.*;
+
+@Tag(name = "그룹 관련 API")
+@RestController
+@RequestMapping("/groups")
+@RequiredArgsConstructor
+public class GroupController {
+
+    private final GroupService groupService;
+
+    @Operation(summary = "그룹 피드 검색")
+    @OkResponse
+    @BadRequestResponse
+    @UnAuthorizedResponse
+    @ForbiddenResponse
+    @NotFoundResponse
+    @InternalServerErrorResponse
+    @GetMapping("/{groupId}/members")
+    public ResponseEntity<GroupMemberSearchResponse> groupSearchFeedResponse(@Login String memberId, @PathVariable String groupId) {
+
+        return ResponseEntity.ok(groupService.searchGroupMembers(memberId, groupId));
+    }
+}
