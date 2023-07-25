c6d6cf7c69c1adbc01882200042c98aa9c0689d6
JunHyeongKim73 <rla5764v@gmail.com>
2023-07-25 12:26:58 +0900

    fix: 회원가입시 미리 생성된 그룹에 참가하도록 변경

--- src/main/java/sleepy/mollu/server/group/repository/GroupRepository.java
+++ src/main/java/sleepy/mollu/server/group/repository/GroupRepository.java
@@ -1,7 +1,13 @@
 package sleepy.mollu.server.group.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import sleepy.mollu.server.group.domain.group.Group;
 
+import java.util.Optional;
+
 public interface GroupRepository extends JpaRepository<Group, String> {
+
+    @Query("select g from Group g where g.name = '디폴트 그룹'")
+    Optional<Group> findDefaultGroup();
 }
