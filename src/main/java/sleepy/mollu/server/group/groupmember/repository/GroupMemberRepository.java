f718a0fef3759311750345239075349a2d79e7b8
JunHyeongKim73 <rla5764v@gmail.com>
2023-07-28 17:07:37 +0900

    fix: 그룹원 피드 조회 API 로직 변경

--- src/main/java/sleepy/mollu/server/group/groupmember/repository/GroupMemberRepository.java
+++ src/main/java/sleepy/mollu/server/group/groupmember/repository/GroupMemberRepository.java
@@ -1,7 +1,14 @@
 package sleepy.mollu.server.group.groupmember.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import sleepy.mollu.server.group.groupmember.domain.GroupMember;
+import sleepy.mollu.server.member.domain.Member;
+
+import java.util.List;
 
 public interface GroupMemberRepository extends JpaRepository<GroupMember, String> {
+
+    @Query("select gm from GroupMember gm join fetch gm.group g where gm.member = :member")
+    List<GroupMember> findAllWithGroupByMember(Member member);
 }
