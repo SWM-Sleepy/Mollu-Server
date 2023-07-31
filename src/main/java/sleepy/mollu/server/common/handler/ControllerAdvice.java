dad191cb40ba1944029c47cf407c76de8ea80a56
JunHyeongKim73 <rla5764v@gmail.com>
2023-07-31 20:09:04 +0900

    fix: `ControllerAdvice`의 예외 처리 방식 변경

--- src/main/java/sleepy/mollu/server/common/handler/ControllerAdvice.java
+++ src/main/java/sleepy/mollu/server/common/handler/ControllerAdvice.java
@@ -15,69 +15,79 @@ import sleepy.mollu.server.common.exception.*;
 import java.util.stream.Collectors;
 
 @RestControllerAdvice
+//@Slf4j
 public class ControllerAdvice {
 
     private static final String BAD_REQUEST_ERROR_MESSAGE = "잘못된 요청입니다.";
-    private final Logger logger = LoggerFactory.getLogger(this.getClass().getSimpleName());
+    private final Logger log = LoggerFactory.getLogger(this.getClass().getSimpleName());
 
     @ExceptionHandler(BadRequestException.class)
     public ResponseEntity<ExceptionResponse> handleBadRequestException(BadRequestException exception) {
-        logger.warn(exception.getMessage());
+        log.error(exception.getClass().getName());
+        log.error(exception.getMessage());
         return ResponseEntity.badRequest().body(new ExceptionResponse(BAD_REQUEST_ERROR_MESSAGE));
     }
 
     @ExceptionHandler(BindException.class)
     public ResponseEntity<ExceptionResponse> handleBindingExceptions(BindException exception) {
+        log.error(exception.getClass().getName());
         final String message = exception.getBindingResult()
                 .getAllErrors()
                 .stream()
                 .map(error -> String.format("%s: %s", ((FieldError) error).getField(), error.getDefaultMessage()))
                 .collect(Collectors.joining(", "));
 
-        logger.warn(message);
+        log.error(message);
 
         return ResponseEntity.badRequest().body(new ExceptionResponse(BAD_REQUEST_ERROR_MESSAGE));
     }
 
     @ExceptionHandler(RuntimeException.class)
     public ResponseEntity<ExceptionResponse> handleRuntimeException(RuntimeException exception) {
-        logger.warn(exception.getMessage());
+        log.error(exception.getClass().getName());
+        log.error(exception.getMessage());
         return ResponseEntity.badRequest().body(new ExceptionResponse(BAD_REQUEST_ERROR_MESSAGE));
     }
 
     @ExceptionHandler(UnAuthenticatedException.class)
     public ResponseEntity<ExceptionResponse> handleAuthenticationException(UnAuthenticatedException exception) {
-        logger.warn(exception.getMessage());
+        log.error(exception.getClass().getName());
+        log.error(exception.getMessage());
         return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(new ExceptionResponse("인증에 실패했습니다."));
     }
 
     @ExceptionHandler(UnAuthorizedException.class)
     public ResponseEntity<ExceptionResponse> handleAuthorizationException(UnAuthorizedException exception) {
-        logger.warn(exception.getMessage());
+        log.error(exception.getClass().getName());
+        log.error(exception.getMessage());
         return ResponseEntity.status(HttpStatus.FORBIDDEN).body(new ExceptionResponse("권한이 없습니다."));
     }
 
     @ExceptionHandler(NotFoundException.class)
     public ResponseEntity<ExceptionResponse> handleNotFoundException(NotFoundException exception) {
-        logger.warn(exception.getMessage());
+        log.error(exception.getClass().getName());
+        log.error(exception.getMessage());
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ExceptionResponse("요청한 리소스를 찾을 수 없습니다."));
     }
 
     @ExceptionHandler(ServletException.class)
     public ResponseEntity<ExceptionResponse> handleServletException(Exception exception) {
-        logger.warn(exception.getMessage());
+        log.error(exception.getClass().getName());
+        log.error(exception.getMessage());
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ExceptionResponse(BAD_REQUEST_ERROR_MESSAGE));
     }
 
     @ExceptionHandler(ServerException.class)
     public ResponseEntity<ExceptionResponse> handleServerException(ServerException exception) {
-        logger.warn(exception.getMessage());
+        log.error(exception.getClass().getName());
+        log.error(exception.getMessage());
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new ExceptionResponse("처리할 수 없는 예외입니다."));
     }
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ExceptionResponse> handleException(Exception exception) {
-        logger.warn(exception.getMessage());
+        log.error(exception.getClass().getName());
+        log.error(exception.getMessage());
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new ExceptionResponse("처리할 수 없는 예외입니다."));
     }
 }
