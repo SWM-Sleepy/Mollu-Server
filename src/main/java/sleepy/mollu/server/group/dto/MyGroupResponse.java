76d6693a00fd5b5c5237f1d8532519775ed9f730
JunHyeongKim73 <rla5764v@gmail.com>
2023-08-04 12:33:34 +0900

    fix: 소속 그룹 조회 응답 변경 및 그룹원 조회 응답 변경

--- src/main/java/sleepy/mollu/server/group/dto/MyGroupResponse.java
+++ src/main/java/sleepy/mollu/server/group/dto/MyGroupResponse.java
@@ -4,6 +4,6 @@ import java.util.List;
 
 public record MyGroupResponse(List<Group> groups) {
 
-    public record Group(String groupId, String groupName) {
+    public record Group(String groupId, String groupName, int groupMemberCount) {
     }
 }
