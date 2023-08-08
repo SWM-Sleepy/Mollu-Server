a9a845aa34ce30f6c5797a1ddef99791817be5da
JunHyeongKim73 <rla5764v@gmail.com>
2023-08-08 18:21:02 +0900

    fix: 그룹원 피드 조회 응답 변경

--- src/main/java/sleepy/mollu/server/content/dto/GroupSearchContentResponse.java
+++ src/main/java/sleepy/mollu/server/content/dto/GroupSearchContentResponse.java
@@ -4,7 +4,7 @@ import java.time.LocalDateTime;
 
 public record GroupSearchContentResponse(Member member, Content content) {
 
-    public record Member(String memberId, String molluId, String memberName, String profileSource) {
+    public record Member(String memberId, String molluId, String name, String profileSource) {
     }
 
     public record Content(String contentId, String location, String groupName, LocalDateTime molluDateTime,
