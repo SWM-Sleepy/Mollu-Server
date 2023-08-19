51842a7d46b8c7addbf4b44b7c2a0b81e8c4a022
JunHyeongKim73 <rla5764v@gmail.com>
2023-08-19 15:21:05 +0900

    chore: MOLLU 타임 알림 TODO 추가

--- src/main/java/sleepy/mollu/server/alarm/dto/FcmAlarmResponse.java
+++ src/main/java/sleepy/mollu/server/alarm/dto/FcmAlarmResponse.java
@@ -2,6 +2,8 @@ package sleepy.mollu.server.alarm.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+
+// TODO: Result 필드 추가
 public record FcmAlarmResponse(@JsonProperty("multicast_id") String multicastId, int success, int failure,
                                @JsonProperty("canonical_ids") String canonicalId) {
 }
