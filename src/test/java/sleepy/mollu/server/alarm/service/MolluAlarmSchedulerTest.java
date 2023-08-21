387cfd640f2fe75cd6cab3500e8baf5cb61b5016
JunHyeongKim73 <rla5764v@gmail.com>
2023-08-21 14:30:17 +0900

    fix: 운영 환경이 아니면 스케줄러 등록되지 않도록 설정

--- src/test/java/sleepy/mollu/server/alarm/service/MolluAlarmSchedulerTest.java
+++ src/test/java/sleepy/mollu/server/alarm/service/MolluAlarmSchedulerTest.java
@@ -4,9 +4,6 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.NullAndEmptySource;
-import org.junit.jupiter.params.provider.ValueSource;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
@@ -14,7 +11,6 @@ import org.springframework.scheduling.TaskScheduler;
 import sleepy.mollu.server.alarm.domain.MolluAlarm;
 import sleepy.mollu.server.alarm.repository.MolluAlarmRepository;
 
-import java.lang.reflect.Field;
 import java.time.Instant;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
@@ -48,39 +44,16 @@ class MolluAlarmSchedulerTest {
     @Mock
     private AlarmService alarmService;
 
-    private void reflect(Object object, String fieldName, Object value) throws NoSuchFieldException, IllegalAccessException {
-        final Field field = object.getClass().getDeclaredField(fieldName);
-        field.setAccessible(true);
-        field.set(object, value);
-    }
-
     @Nested
     @DisplayName("[스케쥴러 실행시] ")
     class SchedulerTest {
 
         final String question = "question";
 
-        @ParameterizedTest
-        @ValueSource(strings = "local")
-        @NullAndEmptySource
-        @DisplayName("프로파일이 `dev` 또는 `prod`가 아니면 실행하지 않는다.")
-        void SchedulerTest(String profile) throws NoSuchFieldException, IllegalAccessException {
-            // given
-            reflect(molluAlarmScheduler, "profile", profile);
-
-            // when
-            molluAlarmScheduler.init();
-
-            // then
-            then(molluAlarmRepository).should(times(0)).findTop();
-        }
-
-        @ParameterizedTest
-        @ValueSource(strings = {"dev", "prod"})
+        @Test
         @DisplayName("어떤 알림도 생성되지 않았으면 새로운 알림을 생성하고, 알림을 예약한다.")
-        void SchedulerTest4(String profile) throws NoSuchFieldException, IllegalAccessException {
+        void SchedulerTest0() {
             // given
-            reflect(molluAlarmScheduler, "profile", profile);
             given(molluAlarmRepository.findTop()).willReturn(Optional.empty());
             given(timePicker.pick(any(LocalTime.class), any(LocalTime.class))).willReturn(LocalTime.now());
             given(picker.pick(anyList())).willReturn(question);
@@ -96,12 +69,10 @@ class MolluAlarmSchedulerTest {
             then(taskScheduler).should(times(1)).schedule(any(Runnable.class), any(Instant.class));
         }
 
-        @ParameterizedTest
-        @ValueSource(strings = {"dev", "prod"})
+        @Test
         @DisplayName("오늘 생성된 알림이 없으면 새로운 알림을 생성한다.")
-        void SchedulerTest2(String profile) throws NoSuchFieldException, IllegalAccessException {
+        void SchedulerTest2() {
             // given
-            reflect(molluAlarmScheduler, "profile", profile);
             final MolluAlarm molluAlarm = mock(MolluAlarm.class);
             given(molluAlarmRepository.findTop()).willReturn(Optional.of(molluAlarm));
             given(molluAlarm.isToday(any())).willReturn(false);
@@ -116,12 +87,10 @@ class MolluAlarmSchedulerTest {
             then(molluAlarmRepository).should(times(1)).save(any(MolluAlarm.class));
         }
 
-        @ParameterizedTest
-        @ValueSource(strings = {"dev", "prod"})
+        @Test
         @DisplayName("오늘 이미 알람을 보냈다면 실행하지 않는다.")
-        void SchedulerTest3(String profile) throws NoSuchFieldException, IllegalAccessException {
+        void SchedulerTest3() {
             // given
-            reflect(molluAlarmScheduler, "profile", profile);
             final MolluAlarm molluAlarm = mock(MolluAlarm.class);
             given(molluAlarmRepository.findTop()).willReturn(Optional.of(molluAlarm));
             given(molluAlarm.isToday(any())).willReturn(true);
@@ -136,9 +105,8 @@ class MolluAlarmSchedulerTest {
 
         @Test
         @DisplayName("알람을 성공적으로 보낸다.")
-        void SchedulerTest4() throws NoSuchFieldException, IllegalAccessException {
+        void SchedulerTest4() {
             // given
-            reflect(molluAlarmScheduler, "profile", "dev");
             final MolluAlarm molluAlarm = mock(MolluAlarm.class);
             given(molluAlarmRepository.findTop()).willReturn(Optional.of(molluAlarm));
             given(molluAlarm.isToday(any())).willReturn(true);
