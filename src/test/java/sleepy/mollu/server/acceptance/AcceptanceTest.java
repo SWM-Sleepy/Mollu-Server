7953081e77aa6266ae53fc45868540e159a4ec7c
JunHyeongKim73 <rla5764v@gmail.com>
2023-09-01 17:10:10 +0900

    chore: 코드 리포맷

--- src/test/java/sleepy/mollu/server/acceptance/AcceptanceTest.java
+++ src/test/java/sleepy/mollu/server/acceptance/AcceptanceTest.java
@@ -34,8 +34,8 @@ public class AcceptanceTest {
     private static final String BASE_URL = "/api";
     private static final String AUTH_URL = BASE_URL + "/auth";
     private static final String MEMBER_URL = BASE_URL + "/members";
-    private static final String CONTENT_URL = BASE_URL + "/contents";
     private static final String MEMBER_EMOJI_URL = MEMBER_URL + "/emojis";
+    private static final String CONTENT_URL = BASE_URL + "/contents";
     @LocalServerPort
     protected int port;
     @SpyBean
--- src/test/java/sleepy/mollu/server/member/emoji/service/MemberEmojiServiceImplTest.java
+++ src/test/java/sleepy/mollu/server/member/emoji/service/MemberEmojiServiceImplTest.java
@@ -20,7 +20,6 @@ import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.BDDMockito.then;
 import static org.mockito.Mockito.mock;
