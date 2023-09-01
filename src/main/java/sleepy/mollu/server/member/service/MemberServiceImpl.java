515265dab3d0cecc90cc5c80a58fb89d5790430e
JunHyeongKim73 <rla5764v@gmail.com>
2023-09-01 10:25:43 +0900

    fix: 내 컨텐츠 조회 API 오류 수정

--- src/main/java/sleepy/mollu/server/member/service/MemberServiceImpl.java
+++ src/main/java/sleepy/mollu/server/member/service/MemberServiceImpl.java
@@ -10,11 +10,11 @@ import sleepy.mollu.server.group.exception.GroupNotFoundException;
 import sleepy.mollu.server.group.repository.GroupRepository;
 import sleepy.mollu.server.member.controller.dto.MyContentsResponse;
 import sleepy.mollu.server.member.domain.Member;
+import sleepy.mollu.server.member.domain.content.SearchRange;
 import sleepy.mollu.server.member.exception.MemberNotFoundException;
 import sleepy.mollu.server.member.repository.MemberRepository;
 
 import java.time.LocalDate;
-import java.time.LocalDateTime;
 import java.util.List;
 
 @Service
@@ -22,22 +22,15 @@ import java.util.List;
 @RequiredArgsConstructor
 public class MemberServiceImpl implements MemberService {
 
-    private static final int FROM_HOUR = 0;
-    private static final int FROM_MINUTE = 0;
-    private static final int TO_HOUR = 23;
-    private static final int TO_MINUTE = 59;
-    private static final int SEARCH_PERIOD = 7;
-
     private final MemberRepository memberRepository;
     private final ContentRepository contentRepository;
     private final GroupRepository groupRepository;
 
     @Override
     public MyContentsResponse searchMyContents(String memberId, LocalDate date) {
-        final Member member = getMember("memberId");
-        final LocalDateTime from = getFromDateTime(date.minusDays(SEARCH_PERIOD));
-        final LocalDateTime to = getToDateTime(date);
-        final List<Content> contents = contentRepository.findAllByMemberAndDate(member, from, to);
+        final Member member = getMember(memberId);
+        final SearchRange range = SearchRange.from(date);
+        final List<Content> contents = contentRepository.findAllByMemberAndDate(member, range.getFrom(), range.getTo());
 
         return getMyContentsResponse(contents);
     }
@@ -45,14 +38,6 @@ public class MemberServiceImpl implements MemberService {
     private Member getMember(String memberId) {
         return memberRepository.findById(memberId)
                 .orElseThrow(() -> new MemberNotFoundException("[" + memberId + "] 에 해당하는 멤버가 없습니다."));
-    }
-
-    private LocalDateTime getFromDateTime(LocalDate date) {
-        return date.atTime(FROM_HOUR, FROM_MINUTE);
-    }
-
-    private LocalDateTime getToDateTime(LocalDate date) {
-        return date.atTime(TO_HOUR, TO_MINUTE);
     }
 
     private Group getGroup() {
