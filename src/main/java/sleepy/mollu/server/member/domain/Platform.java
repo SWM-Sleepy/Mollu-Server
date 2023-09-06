4d81a5f3b2564f1a063fe5799d790411ee757529
JunHyeongKim73 <rla5764v@gmail.com>
2023-09-06 16:39:09 +0900

    [MOL-220] feat: 플랫폼 enum 타입 구현

---
+++ src/main/java/sleepy/mollu/server/member/domain/Platform.java
@@ -0,0 +1,13 @@
+package sleepy.mollu.server.member.domain;
+
+public enum Platform {
+    ANDROID, IOS;
+
+    public static Platform from(String platform) {
+        try {
+            return Platform.valueOf(platform.toUpperCase());
+        } catch (IllegalArgumentException | NullPointerException e) {
+            throw new IllegalArgumentException("[" + platform + "]은 올바르지 않은 플랫폼입니다.");
+        }
+    }
+}
