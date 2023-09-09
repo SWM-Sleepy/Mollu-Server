1ee5a910231cea548e8eff71c1edca2fea5bd58c
JunHyeongKim73 <rla5764v@gmail.com>
2023-09-09 17:45:56 +0900

    fix 알림 토큰 설정 API 변경

--- src/test/java/sleepy/mollu/server/member/domain/PlatformTest.java
+++ src/test/java/sleepy/mollu/server/member/domain/PlatformTest.java
@@ -2,7 +2,7 @@ package sleepy.mollu.server.member.domain;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.junit.jupiter.params.provider.NullSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThatCode;
@@ -11,6 +11,7 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 class PlatformTest {
 
     @ParameterizedTest
+    @NullSource
     @ValueSource(strings = {"android", "ios"})
     @DisplayName("유효한 플랫폼을 입력하면 해당 플랫폼을 반환한다.")
     void PlatformTest0(String type) {
@@ -19,7 +20,6 @@ class PlatformTest {
     }
 
     @ParameterizedTest
-    @NullAndEmptySource
     @ValueSource(strings = {"androids", "iosa"})
     @DisplayName("유효하지 않은 플랫폼을 입력하면 오류가 발생한다.")
     void PlatformTest1(String type) {
