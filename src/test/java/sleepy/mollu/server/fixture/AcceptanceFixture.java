f8a45951e4cc718f46943a4df7bed48e20d2e591
Kim Jun Hyeong <56298375+JunHyeongKim73@users.noreply.github.com>
2023-09-09 17:50:49 +0900

    bug: 알림 토큰 설정 오류를 해결한다. (#144)
    * fix 알림 토큰 설정 API 변경
    
    * test: 알림 토큰 설정 관련 인수 테스트 작성

--- src/test/java/sleepy/mollu/server/fixture/AcceptanceFixture.java
+++ src/test/java/sleepy/mollu/server/fixture/AcceptanceFixture.java
@@ -3,6 +3,7 @@ package sleepy.mollu.server.fixture;
 import sleepy.mollu.server.content.reaction.controller.dto.CreateReactionRequest;
 import sleepy.mollu.server.content.report.dto.ReportRequest;
 import sleepy.mollu.server.member.dto.SignupRequest;
+import sleepy.mollu.server.member.preference.dto.PhoneTokenRequest;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
@@ -10,10 +11,10 @@ import java.time.LocalDateTime;
 public class AcceptanceFixture {
 
     public static final LocalDate NOW_DATE = LocalDate.now();
-    public static final LocalDateTime NOW = LocalDateTime.now();
 
     public static final SignupRequest 회원가입_요청_데이터 = new SignupRequest("name", NOW_DATE, "mollu");
     public static final SignupRequest 회원가입_요청_데이터2 = new SignupRequest("name", NOW_DATE, "mollu1");
+    public static final PhoneTokenRequest 알림_토큰_설정_요청_데이터 = new PhoneTokenRequest("phoneToken", null);
     public static final ReportRequest 신고_요청_데이터 = new ReportRequest("신고 사유");
     public static final CreateReactionRequest 컨텐츠_반응_추가_요청_데이터 = new CreateReactionRequest("emoticon1");
 }
