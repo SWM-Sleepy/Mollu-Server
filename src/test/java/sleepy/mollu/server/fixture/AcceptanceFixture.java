1e59f573f43d8c419d8b1756f9343014438048db
JunHyeongKim73 <rla5764v@gmail.com>
2023-09-09 23:11:30 +0900

    [MOL-222] test: 댓글 작성 인수 테스트 작성

--- src/test/java/sleepy/mollu/server/fixture/AcceptanceFixture.java
+++ src/test/java/sleepy/mollu/server/fixture/AcceptanceFixture.java
@@ -18,5 +18,5 @@ public class AcceptanceFixture {
     public static final PhoneTokenRequest 알림_토큰_설정_요청_데이터 = new PhoneTokenRequest("phoneToken", null);
     public static final ReportRequest 신고_요청_데이터 = new ReportRequest("신고 사유");
     public static final CreateReactionRequest 컨텐츠_반응_추가_요청_데이터 = new CreateReactionRequest("emoticon1");
-    public static final CreateCommentRequest 댓글_등록_요청_데이터 = new CreateCommentRequest("댓글 내용");
+    public static final CreateCommentRequest 댓글_등록_요청_데이터 = new CreateCommentRequest("김".repeat(200));
 }
