61217bf95065bead1bf9318f2cb078d62de9c4fb
JunHyeongKim73 <rla5764v@gmail.com>
2023-09-15 17:58:59 +0900

    [MOL-207] fix: Introduction 클래스 제약 구현

--- src/main/java/sleepy/mollu/server/group/domain/group/Introduction.java
+++ src/main/java/sleepy/mollu/server/group/domain/group/Introduction.java
@@ -10,11 +10,33 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class Introduction {
 
+    private static final String DEFAULT_INTRODUCTION = "";
+    private static final int INTRODUCTION_MAX_LENGTH = 20;
+
     @Column(name = "introduction")
-    private String value;
+    private String value = DEFAULT_INTRODUCTION;
 
     public Introduction(String value) {
+        if (checkNull(value)) {
+            return;
+        }
 
+        validateLength(value);
         this.value = value;
+    }
+
+    private boolean checkNull(String value) {
+        if (value == null) {
+            this.value = "";
+            return true;
+        }
+
+        return false;
+    }
+
+    private void validateLength(String value) {
+        if (value.length() > INTRODUCTION_MAX_LENGTH) {
+            throw new IllegalArgumentException("소개글은 최대 " + INTRODUCTION_MAX_LENGTH + "자까지 입력 가능합니다.");
+        }
     }
 }
---
+++ src/test/java/sleepy/mollu/server/group/domain/group/IntroductionTest.java
@@ -0,0 +1,51 @@
+package sleepy.mollu.server.group.domain.group;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.*;
+
+class IntroductionTest {
+
+    public static Stream<Arguments> validSource() {
+        return Stream.of(
+                Arguments.of("소"),
+                Arguments.of("소".repeat(20))
+        );
+    }
+
+    public static Stream<Arguments> inValidSource() {
+        return Stream.of(
+                Arguments.of("소".repeat(21))
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("validSource")
+    @DisplayName("[Introduction 객체 생성시] 유효한 소개글로 객체를 생성할 수 있다.")
+    void IntroductionTest0(String value) {
+        assertThatCode(() -> new Introduction(value)).doesNotThrowAnyException();
+    }
+
+    @ParameterizedTest
+    @MethodSource("inValidSource")
+    @DisplayName("[Introduction 객체 생성시] 유효하지 않은 소개글로 객체를 생성할 수 없다.")
+    void IntroductionTest1(String value) {
+        assertThatThrownBy(() -> new Introduction(value)).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    @DisplayName("[Introduction 객체 생성시] 소개글이 없으면 빈 문자열로 객체가 생성된다.")
+    void IntroductionTest2() {
+        // given & when
+        final Introduction introduction = new Introduction(null);
+
+        // then
+        assertThat(introduction.getValue()).isEmpty();
+    }
+}
