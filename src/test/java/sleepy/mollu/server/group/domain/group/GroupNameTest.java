7a6f2d8299be60ae6f0a638fe5638ce2acc09f64
JunHyeongKim73 <rla5764v@gmail.com>
2023-09-15 18:04:39 +0900

    [MOL-207] fix: GroupName 클래스 제약 구현

---
+++ src/test/java/sleepy/mollu/server/group/domain/group/GroupNameTest.java
@@ -0,0 +1,43 @@
+package sleepy.mollu.server.group.domain.group;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.*;
+
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThatCode;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class GroupNameTest {
+
+    public static Stream<Arguments> validSource() {
+        return Stream.of(
+                Arguments.of("그".repeat(15)),
+                Arguments.of("a".repeat(15)),
+                Arguments.of("a")
+        );
+    }
+
+    public static Stream<Arguments> inValidSource() {
+        return Stream.of(
+                Arguments.of("그".repeat(16)),
+                Arguments.of("a".repeat(16))
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("validSource")
+    @DisplayName("[GroupName 객체 생성시] 유효한 그룹 이름이 주어지면, 객체가 생성된다.")
+    void GroupNameTest0(String value) {
+        assertThatCode(() -> new GroupName(value)).doesNotThrowAnyException();
+    }
+
+    @ParameterizedTest
+    @MethodSource("inValidSource")
+    @NullAndEmptySource
+    @DisplayName("[GroupName 객체 생성시] 유효하지 않은 그룹 이름이 주어지면, 객체가 생성이 실패한다.")
+    void GroupNameTest1(String value) {
+        assertThatThrownBy(() -> new GroupName(value)).isInstanceOf(IllegalArgumentException.class);
+    }
+}
