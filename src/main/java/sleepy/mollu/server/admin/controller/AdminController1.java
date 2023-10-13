89ab28e51ab0d868feaae05925ec04b212b93f41
JunHyeongKim73 <rla5764v@gmail.com>
2023-10-13 15:57:30 +0900

    feat: 템플릿 페이지 추가

---
+++ src/main/java/sleepy/mollu/server/admin/controller/AdminController1.java
@@ -0,0 +1,26 @@
+package sleepy.mollu.server.admin.controller;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import sleepy.mollu.server.swagger.InternalServerErrorResponse;
+import sleepy.mollu.server.swagger.OkResponse;
+
+@Tag(name = "관리자")
+@Controller
+@RequestMapping("/admin")
+@RequiredArgsConstructor
+public class AdminController1 {
+
+    @Operation(summary = "관리자 로그인 페이지")
+    @OkResponse
+    @InternalServerErrorResponse
+    @GetMapping("/dashboard")
+    public String getDashboard() {
+
+        return "pages/dashboard";
+    }
+}
