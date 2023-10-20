cb8368ceed40010b1bbf9c2fafdf6b7f26c7ad6a
Kim Jun Hyeong <56298375+JunHyeongKim73@users.noreply.github.com>
2023-10-20 22:49:08 +0900

    fix: `Comment` 엔티티에 제약 조건 추가 (#193)

--- src/main/resources/db/migration/V1.7__comment_add_on_delete_cascade_constraint.sql
+++ src/main/resources/db/migration/V1.7__comment_add_on_delete_cascade_constraint.sql
@@ -1,6 +1,5 @@
-ALTER TABLE comment DROP CONSTRAINT FKmrrrpi513ssu63i2783jyiv9m;
 ALTER TABLE comment
     ADD CONSTRAINT FKmrrrpi513ssu63i2783jyiv9m FOREIGN KEY (content_id) REFERENCES content (content_id) ON DELETE CASCADE;
 
-ALTER TABLE comment DROP CONSTRAINT FKr9jchxqv43o7l4tv7qkoqr4di;
-ALTER TABLE comment ADD CONSTRAINT FKr9jchxqv43o7l4tv7qkoqr4di FOREIGN KEY (member_id) REFERENCES member (member_id) ON DELETE CASCADE;
+ALTER TABLE comment
+    ADD CONSTRAINT FKr9jchxqv43o7l4tv7qkoqr4di FOREIGN KEY (member_id) REFERENCES member (member_id) ON DELETE CASCADE;
