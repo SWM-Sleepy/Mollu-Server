b3486da45c56b183d428a81e094df59ae9d76a50
Kim Jun Hyeong <56298375+JunHyeongKim73@users.noreply.github.com>
2023-10-20 23:25:16 +0900

    bug: `Comment` 엔티티 제약 조건 오류 해결 (#194)

--- src/main/resources/db/migration/V1.7__comment_add_on_delete_cascade_constraint.sql
+++ src/main/resources/db/migration/V1.7__comment_add_on_delete_cascade_constraint.sql
@@ -1,5 +1,5 @@
 ALTER TABLE comment
-    ADD CONSTRAINT FKmrrrpi513ssu63i2783jyiv9m FOREIGN KEY (content_id) REFERENCES content (content_id) ON DELETE CASCADE;
+    ADD CONSTRAINT FOREIGN KEY (content_id) REFERENCES content (content_id) ON DELETE CASCADE;
 
 ALTER TABLE comment
-    ADD CONSTRAINT FKr9jchxqv43o7l4tv7qkoqr4di FOREIGN KEY (member_id) REFERENCES member (member_id) ON DELETE CASCADE;
+    ADD CONSTRAINT FOREIGN KEY (member_id) REFERENCES member (member_id) ON DELETE CASCADE;
