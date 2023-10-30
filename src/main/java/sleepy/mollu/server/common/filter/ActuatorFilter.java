d3a91ca67c84ea43ea4a844dc68bb57578eac093
JunHyeongKim73 <rla5764v@gmail.com>
2023-10-30 14:36:02 +0900

    fix: 클라이언트 IP 로깅 추가

--- src/main/java/sleepy/mollu/server/common/filter/ActuatorFilter.java
+++ src/main/java/sleepy/mollu/server/common/filter/ActuatorFilter.java
@@ -3,12 +3,14 @@ package sleepy.mollu.server.common.filter;
 import jakarta.servlet.*;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
 import java.util.List;
 
 @Component
+@Slf4j
 public class ActuatorFilter implements Filter {
 
     private static final List<String> ALLOWED_IP_ADDRESSES = List.of("127.0.0.1", "0:0:0:0:0:0:0:1");
@@ -22,7 +24,9 @@ public class ActuatorFilter implements Filter {
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
         final HttpServletRequest httpServletRequest = (HttpServletRequest) request;
         final HttpServletResponse httpServletResponse = (HttpServletResponse) response;
-        final String remoteAddr = httpServletRequest.getRemoteAddr();
+        final String remoteAddr = getClientIpAddr(httpServletRequest);
+
+        log.info("Remote address: {}", remoteAddr);
         if (!ALLOWED_IP_ADDRESSES.contains(remoteAddr)) {
             httpServletResponse.sendError(HttpServletResponse.SC_FORBIDDEN);
         }
@@ -33,5 +37,33 @@ public class ActuatorFilter implements Filter {
     @Override
     public void destroy() {
         Filter.super.destroy();
+    }
+
+    private String getClientIpAddr(HttpServletRequest request) {
+
+        if (request == null) {
+            return "";
+        }
+
+        String ip = request.getHeader("X-Forwarded-For");
+
+        if (ip == null || ip.isEmpty() || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("Proxy-Client-IP");
+        }
+        if (ip == null || ip.isEmpty() || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("WL-Proxy-Client-IP");
+        }
+        if (ip == null || ip.isEmpty() || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("HTTP_CLIENT_IP");
+        }
+        if (ip == null || ip.isEmpty() || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("HTTP_X_FORWARDED_FOR");
+        }
+        if (ip == null || ip.isEmpty() || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getRemoteAddr();
+        }
+
+        return ip;
+
     }
 }
